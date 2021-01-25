b7f12777040e265920bea598bf3aa9f1c6d72c01
choitaehoon <xognsdl1230@naver.com>
2021-01-25 12:31:03 +0900

    test gogo

---
+++ src/test/java/com/test/TestCi.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TestCi {
+
+    @Test
+    public void test() {
+        assertThat(3, is(3));
+    }
+
+}
